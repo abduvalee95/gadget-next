5a52a01fe6b95b43bba2e3f070f9f93e2925647a
abduvalee95 <abduvalee95@gmail.com>
2024-06-22 04:14:13 +0900

    fix: image loading Circular progress logic

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { Box, Button, Checkbox, Stack, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, Stack, Typography } from '@mui/material';
 import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
 import withLayoutFull from '../../libs/components/layout/LayoutFull';
 import { NextPage } from 'next';
@@ -73,7 +73,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		error: getPropertyError,
 		refetch: getPropertyRefetch,
 	} = useQuery(GET_PROPERTY, {
-		fetchPolicy: 'cache-and-network',
+		fetchPolicy: 'network-only',
 		variables: { input: propertyId },
 		//propertyni qayta qayta olormasligi uchun < Boshlangich qiymati 'null' boladi  agar qiymati mavjud bolmasa Queryni Skip qilgin
 		skip: !propertyId,
@@ -121,7 +121,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		fetchPolicy: 'cache-and-network',
 		variables: { input: initialComment },
 		//Malumotlar qachon olinish kerak
-		
+
 		skip: !commentInquiry.search.commentRefId, // Agar  " " bolsa amalga oshmasin. "fdjkk" bolsa ishladi; "" bolganda falsy boladi  skip true qiymatni qabul qiladi 
 		notifyOnNetworkStatusChange: true,
 		onCompleted: (data: T) => {
@@ -207,6 +207,16 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 			await sweetErrorHandling(error);
 		}
 	};
+
+	if (getPropertiesLoading) {
+		return (
+			<Stack sx={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%', height:'1080px'}}>
+				<CircularProgress size={"4rem"}/>
+		</Stack>
+	)
+}
+
+
 
 	if (device === 'mobile') {
 		return <div>PROPERTY DETAIL PAGE</div>;
@@ -302,7 +312,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 							<Stack className={'images'}>
 								<Stack className={'main-image'}>
 									<img
-										src={slideImage ? `${REACT_APP_API_URL}/${slideImage}` : '/img/property/bigImage.png'}
+										src={slideImage ? `${REACT_APP_API_URL}/${slideImage}` : ''}
 										alt={'main-image'}
 									/>
 								</Stack>
